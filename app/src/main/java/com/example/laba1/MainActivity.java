7c4d1a6e9f4c365beedff439a450936bf9913cc1
marta <martavasiukovich@mail.ru>
2020-09-08 08:58:48 +0300

    1

--- app/src/main/java/com/example/laba1/MainActivity.java
+++ app/src/main/java/com/example/laba1/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.laba1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.example.laba1.test.TextFunction;
 
 public class MainActivity extends AppCompatActivity {
 //TODO Privet
@@ -10,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        for (int count = 0; count < 10; count++) {     Log.d("MainActivity", " counter =" + count); }
+        displayText();
     }
+
+    private void displayText() {
+        TextFunction tf = new TextFunction();
+        TextView nt = findViewById(R.id.newtest);
+        nt.setText(tf.getValue());
+    }
+
+
 }
---
+++ app/src/main/java/com/example/laba1/test/TextFunction.java
@@ -0,0 +1,6 @@
+package com.example.laba1.test;
+
+public class TextFunction {
+    public  String getValue()
+    {         return "Hello from first project";     }
+}
